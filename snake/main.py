c072d18e597eff91ee08bd9dd40a1a15d6550c1b
Ernesto Muñoz <ojizaki@gmail.com>
2022-04-16 01:46:52 +0200

    added a fps counter

--- snake/main.py
+++ snake/main.py
@@ -7,7 +7,7 @@ from pygame.locals import *
 
 from snake import Snake
 
-FPS = 60
+FPS = 0
 FPS_CLOCK = pygame.time.Clock()
 
 
@@ -20,6 +20,7 @@ class App:
         self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
         pygame.display.set_caption("Snake")
         self._font = pygame.font.Font("freesansbold.ttf", 48)
+        self._fps_font = pygame.font.Font("freesansbold.ttf", 18)
         self._running = True
 
         self.snake = Snake()
@@ -39,6 +40,10 @@ class App:
         self._display_surf.blit(pygame.transform.scale(screen, self._display_surf.get_rect().size), (0, 0))
         score = self._font.render(f"Score: {self.snake.get_score()}", True, (80, 130, 210))
         self._display_surf.blit(score, (10, 10))
+
+        fps = self._fps_font.render(f"fps: {FPS_CLOCK.get_fps():.1f}", True, (200, 10, 10))
+        fps_size = fps.get_size()
+        self._display_surf.blit(fps, (0, self.height - fps_size[1]))
 
         pygame.display.update()
 
